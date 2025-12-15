687619d7db919a295106a527f87060d46077f9ad
moop250 <hugo.libine@proton.me>
2025-12-15 19:24:42 +0100

    conforming more to the norm

--- srcs/frontend/vite.config.ts
+++ srcs/frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig ({
-	root: "public",
+	root: ".",
 	base: "/",
-	plugins: [
-		tailwindcss(),
-	],
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+	},
 })
