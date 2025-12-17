66a97ae8d33320b8a9bbcf9347f7683b96f729df
njeanbou <njeanbou@CocoDebian.njeanbou.ch>
2025-12-17 07:10:21 +0100

    Pong fonctionnel et adapte pour l'integration au projet

--- pong/frontend/src/game/pong.ts
+++ pong/frontend/src/game/pong.ts
@@ -18,7 +18,6 @@ export type PongConfig = {
 	paddleWidth:	number;
 	paddleHeight:	number;
 	paddleMargin:	number;
-	maxBounceAngle:	number;
 	winningScore:	number;
 };
 
@@ -68,12 +67,11 @@ export type PongState = {
 
 export const DEFAULT_CONFIG: PongConfig = {
 	ballRadius:		10,
-	paddleSpeed:	6,
-	ballSpeed:		4.2,
+	paddleSpeed:	600,
+	ballSpeed:		420,
 	paddleWidth:	10,
 	paddleHeight:	150,
 	paddleMargin:	10,
-	maxBounceAngle:	8,
 	winningScore:	9,
 };
 
@@ -200,14 +198,14 @@ function applyScore(state: PongState, cfg: PongConfig, event?: PongEvents, playe
 	event?.onScore?.(player, state.scoreP1, state.scoreP2);
 
 	if (state.scoreP1 >= cfg.winningScore) {
-		state.phase == "GAMEOVER";
+		state.phase = "GAMEOVER";
 		state.winner = 1;
 		event?.onGameOver?.(1, state.scoreP1, state.scoreP2);
 		event?.onStateChange?.(state.phase);
 		return;
 	}
-	if (state.scoreP1 >= cfg.winningScore) {
-		state.phase == "GAMEOVER";
+	if (state.scoreP2 >= cfg.winningScore) {
+		state.phase = "GAMEOVER";
 		state.winner = 2;
 		event?.onGameOver?.(2, state.scoreP1, state.scoreP2);
 		event?.onStateChange?.(state.phase);
@@ -234,7 +232,7 @@ function update(state: PongState, input: PongInput, dt: number, cfg: PongConfig,
 			resetBall(state, cfg);
 			event?.onStateChange?.(state.phase);
 		}
-		return;
+		// return;
 	}
 
 	if (state.phase === "GAMEOVER") {
@@ -279,44 +277,28 @@ function update(state: PongState, input: PongInput, dt: number, cfg: PongConfig,
 				event?.onStateChange?.(state.phase);
 			}
 		}
+		if (togglePause) {
+				state.phase = "PAUSED";
+				event?.onStateChange?.(state.phase);
+			}
 		return;
 	}
 
 
 	// RUNNING
-	//paddles
-	const moveP1 = (input.p1.down ? 1 : 0) - (input.p1.up ? 1 : 0);
-	const moveP2 = (input.p2.down ? 1 : 0) - (input.p2.up ? 1 : 0);
-
-	state.p1Y += moveP1 * cfg.paddleSpeed * dt;
-	state.p2Y += moveP2 * cfg.paddleSpeed * dt;
-
-	state.p1Y = clamp(state.p1Y, 0, state.height - cfg.paddleHeight);
-	state.p2Y = clamp(state.p2Y, 0, state.height - cfg.paddleHeight);
-
-	//ball
-	state.ballX += state.ballVX * dt;
-	state.ballY += state.ballVY * dt;
 
 	// top/bottom bounce
 	if (state.ballY <= cfg.ballRadius) {
 		state.ballY = cfg.ballRadius;
 		state.ballVY *= -1;
 	}
-	if (state.ballY >= state.width - cfg.ballRadius) {
+	if (state.ballY >= state.height - cfg.ballRadius) {
 		state.ballY = state.height - cfg.ballRadius;
 		state.ballVY *= -1;
 	}
-
-	//scoring
-	if (state.ballX >= state.width - cfg.ballRadius) {
-		applyScore(state, cfg, event, 1);
-		return;
-	}
-	if (state.ballX <= cfg.ballRadius) {
-		applyScore(state, cfg, event, 2);
-		return;
-	}
+	//ball
+	state.ballX += state.ballVX * dt;
+	state.ballY += state.ballVY * dt;
 
 	//collisions paddles
 	const p1X = cfg.paddleMargin;
@@ -331,7 +313,7 @@ function update(state: PongState, input: PongInput, dt: number, cfg: PongConfig,
 	if (hitP1) {
 		const rel = (state.p1Y + cfg.paddleHeight / 2) - state.ballY;
 		const norm = rel / (cfg.paddleHeight / 2);
-		state.ballVY = -norm * cfg.maxBounceAngle * 60;
+		state.ballVY = -norm * (cfg.ballSpeed * 1.2);
 		state.ballVX *= -1;
 		state.ballX = p1X + cfg.paddleWidth + cfg.ballRadius;
 	}
@@ -345,9 +327,42 @@ function update(state: PongState, input: PongInput, dt: number, cfg: PongConfig,
 	if (hitP2) {
 		const rel = (state.p2Y + cfg.paddleHeight / 2) - state.ballY;
 		const norm = rel / (cfg.paddleHeight / 2);
-		state.ballVY = -norm * cfg.maxBounceAngle * 60;
+		state.ballVY = -norm * (cfg.ballSpeed * 1.2);
 		state.ballVX *= -1;
 		state.ballX = p2X - cfg.ballRadius;
+	}
+
+	if (state.phase === "LOBBY")
+	{
+		if (state.ballX <= cfg.ballRadius) {
+			state.ballX = cfg.ballRadius;
+			state.ballVX *= -1;
+		}
+		if (state.ballX >= state.width - cfg.ballRadius) {
+			state.ballX = state.width - cfg.ballRadius;
+			state.ballVX *= -1;
+		}
+	}
+	if (state.phase === "RUNNING") {
+		//paddles
+		const moveP1 = (input.p1.down ? 1 : 0) - (input.p1.up ? 1 : 0);
+		const moveP2 = (input.p2.down ? 1 : 0) - (input.p2.up ? 1 : 0);
+
+		state.p1Y += moveP1 * cfg.paddleSpeed * dt;
+		state.p2Y += moveP2 * cfg.paddleSpeed * dt;
+
+		state.p1Y = clamp(state.p1Y, 0, state.height - cfg.paddleHeight);
+		state.p2Y = clamp(state.p2Y, 0, state.height - cfg.paddleHeight);	
+
+		//scoring
+		if (state.ballX >= state.width - cfg.ballRadius) {
+			applyScore(state, cfg, event, 1);
+			return;
+		}
+		if (state.ballX <= cfg.ballRadius) {
+			applyScore(state, cfg, event, 2);
+			return;
+		}
 	}
 }
 
@@ -361,9 +376,17 @@ function creatKeyMap(): KeyMap {
 	return (Object.create(null));
 }
 
-function bindKeyboard(keys: KeyMap) {
-	const down = (e: KeyboardEvent) => (keys[e.key] = true);
-	const up = (e: KeyboardEvent) => (keys[e.key] = false);
+function bindKeyboard(keysDown: KeyMap, keyPressed: KeyMap) {
+	const down = (e: KeyboardEvent) => { 
+		if (!keysDown[e.key] && !e.repeat) {
+			keyPressed[e.key] = true;
+		}
+		keysDown[e.key] = true;
+	};
+		
+	const up = (e: KeyboardEvent) => {
+		keysDown[e.key] = false;
+	}
 
 	window.addEventListener("keydown", down);
 	window.addEventListener("keyup", up);
@@ -375,21 +398,26 @@ function bindKeyboard(keys: KeyMap) {
 }
 
 //mapage clavier (injectable)
-function keyboardToInput(keys: KeyMap): PongInput {
-	return {
+function keyboardToInput(keysDown: KeyMap, keysPressed: KeyMap): PongInput {
+	const input: PongInput = {
 		p1: {
-			up: !!keys["w"],
-			down: !!keys["s"],
-			start: !!keys["Enter"],
-			togglePause: !!keys["Escape"],
+			up: !!keysDown["w"],
+			down: !!keysDown["s"],
+			start: !!keysPressed["Enter"],
+			togglePause: !!keysPressed["Escape"],
 		},
 		p2: {
-			up: !!keys["ArrowUp"],
-			down: !!keys["ArrowDown"],
-			start: !!keys["Enter"],
-			togglePause: !!keys["Escape"],
+			up: !!keysDown["ArrowUp"],
+			down: !!keysDown["ArrowDown"],
+			start: !!keysPressed["Enter"],
+			togglePause: !!keysPressed["Escape"],
 		},
 	};
+
+	keysPressed["Enter"] = false;
+	keysPressed["Escape"] = false;
+
+	return (input);
 }
 
 
@@ -414,10 +442,11 @@ export function startPong(
 	const state = creatInitialState(canvas.width, canvas.height, cfg);
 
 	//default input
-	const keys = creatKeyMap();
-	const unbind = bindKeyboard(keys);
+	const keysDown = creatKeyMap();
+	const keysPressed = creatKeyMap();
+	const unbind = bindKeyboard(keysDown, keysPressed);
 
-	let inputSource: () => PongInput = () => keyboardToInput(keys);
+	let inputSource: () => PongInput = () => keyboardToInput(keysDown, keysPressed);
 
 	//boucle controllable
 	let rafId = 0;
@@ -427,8 +456,8 @@ export function startPong(
 	function loop(now: number) {
 		if (!running)
 			return;
-		const dt = Math.min(0.5, (now - last) / 1000); // clamp dt evite gros saut
-		last = dt;
+		const dt = Math.min(0.05, (now - last) / 1000); // clamp dt evite gros saut
+		last = now;
 
 		const input = inputSource();
 		update(state, input, dt, cfg, events);
@@ -451,7 +480,7 @@ export function startPong(
 			state.width = w;
 			state.height = h;
 			state.p1Y = clamp(state.p1Y, 0, h - cfg.paddleHeight);
-			state.p2Y = clamp(state.p2Y, 0, h = cfg.paddleHeight);
+			state.p2Y = clamp(state.p2Y, 0, h - cfg.paddleHeight);
 			state.ballX = clamp(state.ballX, cfg.ballRadius, w - cfg.ballRadius);
 			state.ballY = clamp(state.ballY, cfg.ballRadius, h - cfg.ballRadius);
 		},
@@ -464,444 +493,3 @@ export function startPong(
 	};
 }
 
-
-
-
-
-
-
-
-
-
-// ===================================================================================================================================================
-
-// const keys: Record<string, boolean> = {};
-
-
-// function draw_number_p1(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, score: Number)
-// {
-// 	ctx.font = "150px 'VT323'";
-// 		ctx.textAlign = "center";
-// 		ctx.fillText(score.toString(), canvas.width / 2 - 100, 100);
-// }
-
-// function draw_number_p2(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, score: Number)
-// {
-// 	ctx.font = "150px 'VT323'";
-// 		ctx.textAlign = "center";
-// 		ctx.fillText(score.toString(), canvas.width / 2 + 100, 100);
-// }
-
-// function draw_score(
-//     ctx: CanvasRenderingContext2D,
-//     canvas: HTMLCanvasElement,
-//     score1: number,
-//     score2: number
-// ) {
-//     switch (score1)
-// 	{
-// 		case 0:
-// 			draw_number_p1(ctx, canvas, 0);
-// 			break;
-// 		case 1:
-// 			draw_number_p1(ctx, canvas, 1);
-// 			break;
-// 		case 2:
-// 			draw_number_p1(ctx, canvas, 2);
-// 			break;
-// 		case 3:
-// 			draw_number_p1(ctx, canvas, 3);
-// 			break;
-// 		case 4:
-// 			draw_number_p1(ctx, canvas, 4);
-// 			break;
-// 		case 5:
-// 			draw_number_p1(ctx, canvas, 5);
-// 			break;
-// 		case 6:
-// 			draw_number_p1(ctx, canvas, 6);
-// 			break;
-// 		case 7:
-// 			draw_number_p1(ctx, canvas, 7);
-// 			break;
-// 		case 8:
-// 			draw_number_p1(ctx, canvas, 8);
-// 			break;
-// 		case 9:
-// 			draw_number_p1(ctx, canvas, 9);
-// 			break;
-// 	}
-// 	switch (score2)
-// 	{
-// 		case 0:
-// 			draw_number_p2(ctx, canvas, 0);
-// 			break;
-// 		case 1:
-// 			draw_number_p2(ctx, canvas, 1);
-// 			break;
-// 		case 2:
-// 			draw_number_p2(ctx, canvas, 2);
-// 			break;
-// 		case 3:
-// 			draw_number_p2(ctx, canvas, 3);
-// 			break;
-// 		case 4:
-// 			draw_number_p2(ctx, canvas, 4);
-// 			break;
-// 		case 5:
-// 			draw_number_p2(ctx, canvas, 5);
-// 			break;
-// 		case 6:
-// 			draw_number_p2(ctx, canvas, 6);
-// 			break;
-// 		case 7:
-// 			draw_number_p2(ctx, canvas, 7);
-// 			break;
-// 		case 8:
-// 			draw_number_p2(ctx, canvas, 8);
-// 			break;
-// 		case 9:
-// 			draw_number_p2(ctx, canvas, 9);
-// 			break;
-// 	}
-// }
-
-// export function startPong(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) 
-// {
-//     const ballRadius = 10;
-//     const padSpeed = 8;
-//     const maxBounceAngle = 8;
-
-// 	let gameOver = false;
-// 	let gameStarted = false;
-// 	let paused = false;
-// 	let resuming = false;
-// 	let countdown = 0;
-// 	let nextCountdownTime = 0;
-// 	let winner: 1 | 2 | null = null;
-
-// 	let scoreP1 = 0;
-// 	let scoreP2 = 0;
-
-//     let ballX = canvas.width / 2;
-//     let ballY = canvas.height / 2;
-
-//     let speedX = 6;
-//     let speedY = 6;
-
-    
-//     let pad1W = 10;
-//     let pad1H = 150;
-    
-//     let pad1X = 10;
-//     let pad1Y = canvas.height / 2 - pad1H / 2;
-
-//     let pad2W = 10;
-//     let pad2H = 150;
-    
-//     let pad2X = canvas.width - (pad2W + 10);
-//     let pad2Y = canvas.height / 2 - pad1H / 2;
-
-// 	if (Math.random() < 0.5)
-// 		speedX *= -1;
-// 	if (Math.random() < 0.5)
-// 		speedY *= -1;
-
-// 	document.addEventListener("keydown", (e) => {
-// 		keys[e.key] = true;
-	
-// 		if (e.key === "Escape") 
-// 		{
-// 			if (!paused && !resuming && gameStarted && !gameOver)
-// 				paused = !paused;
-// 			else if (paused && !resuming && gameStarted && !gameOver)
-// 			{
-// 				paused = false;
-// 				resuming = true;
-// 				countdown = 3;
-// 				nextCountdownTime = performance.now() + 1000;
-// 			}
-// 		}
-
-// 		if (e.key === "Enter")
-// 		{
-// 			gameStarted = true;
-// 			resuming = true;
-// 			countdown = 3;
-// 			nextCountdownTime = performance.now() + 1000;
-// 			ballX = canvas.width / 2;
-//     		ballY = canvas.height / 2;
-// 		}
-
-// 	})
-	
-// 	document.addEventListener("keyup", (e) =>{
-// 		keys[e.key] = false;
-// 	})
-
-// 	function score(player: 1 | 2)
-// 	{
-//         if (player === 1)
-//             scoreP1++;
-//         else
-//             scoreP2++;
-
-// 		if (scoreP1 >= 9)
-// 		{
-// 			gameOver = true;
-// 			winner = 1;
-// 			return ;
-// 		}
-// 		if (scoreP2 >= 9)
-// 		{
-// 			gameOver = true;
-// 			winner = 2;
-// 			return ;
-// 		}
-
-//         // reset balle
-//         ballX = canvas.width / 2;
-//         ballY = canvas.height / 2;
-
-//         // direction aléatoire
-//         speedX = 6;
-//         speedY = 6;
-//         if (Math.random() < 0.5)
-//             speedX *= -1;
-//         if (Math.random() < 0.5)
-//             speedY *= -1;
-//     }
-
-// 	function drawLoader()
-// 	{
-// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-// 		ballX += speedX;
-//         ballY += speedY;
-
-		
-
-// 		if (ballX >= canvas.width - ballRadius || ballX <= 0 + ballRadius)
-//             speedX *= -1;
-// 		if (ballY <= 0 + ballRadius || ballY >= canvas.height - ballRadius)
-//             speedY *= -1;
-
-// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-// 		ctx.fillStyle = "grey";
-//         ctx.beginPath();
-
-//         for (let i = 0; i < canvas.height; i += 20)
-//         {
-//             ctx.rect(canvas.width / 2 - 5 / 2, i, 10, 10)
-//         }
-// 		ctx.fill();
-
-// 		ctx.fillStyle = "white"
-// 		ctx.beginPath();
-
-//         ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
-//         ctx.fill();
-// 		ctx.font = "100px 'VT323'";
-// 		ctx.textAlign = "center";
-// 		ctx.fillText("Press Enter For Start Game", canvas.width / 2, canvas.height / 2);
-// 		draw_score(ctx, canvas, scoreP1, scoreP2);
-// 		// if (keys["Enter"])
-// 		// {
-// 		// 	gameStarted = true;
-// 		// 	resuming = true;
-// 		// 	ballX = canvas.width / 2;
-//     	// 	ballY = canvas.height / 2;
-// 		// 	return ;
-// 		// }
-// 	}
-
-// 	function botPalyer()
-// 	{
-// 		if (pad1Y + (pad1H / 2) > pad2Y + (pad2H / 2))
-// 		{
-// 			if (ballY <= pad2Y + (pad2H / 4))
-// 				pad2Y -= padSpeed;
-// 			else if (ballY > pad2Y + (pad2H / 4))
-// 				pad2Y += padSpeed;
-// 		}
-// 		else
-// 		{
-// 			if (ballY <= pad2Y + (pad2H / 4) * 3)
-// 				pad2Y -= padSpeed;
-// 			else if (ballY > pad2Y + (pad2H / 4) * 3)
-// 				pad2Y += padSpeed;
-// 		}
-					
-// 	}
-
-//     function drawGame()
-//     {
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-// 		if (gameOver)
-// 		{
-// 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-// 			draw_score(ctx, canvas, scoreP1, scoreP2);
-// 			ctx.fillStyle = "white";
-// 			ctx.font = "150px 'VT323'";
-// 			ctx.textAlign = "center";
-
-// 			const text = winner === 1 ? "PLAYER 1 WINS!" : "PLAYER 2 WINS!";
-// 			ctx.fillText(text, canvas.width / 2, canvas.height / 2);
-// 			if (keys["Enter"])
-// 			{
-// 				gameOver = false;
-// 				scoreP1 = 0;
-// 				scoreP2 = 0;
-// 			}
-// 			return;
-// 		}
-
-// 		if (paused)
-// 		{
-// 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-// 			draw_score(ctx, canvas, scoreP1, scoreP2);
-	
-// 			ctx.fillStyle = "white";
-// 			ctx.font = "120px 'VT323'";
-// 			ctx.textAlign = "center";
-// 			ctx.fillText("PAUSED", canvas.width / 2, canvas.height / 2);
-	
-// 			return;
-// 		}
-		
-// 		if (resuming)
-// 		{
-// 			const now = performance.now();
-// 			if (now >= nextCountdownTime) {
-// 				countdown--;
-// 				nextCountdownTime = now + 1000;
-// 			}
-	
-// 			if (countdown <= 0) {
-// 				resuming = false;
-// 			} else {
-// 				draw_score(ctx, canvas, scoreP1, scoreP2);
-	
-// 				ctx.fillStyle = "white";
-// 				ctx.font = "150px 'VT323'";
-// 				ctx.textAlign = "center";
-// 				ctx.fillText(`${countdown}`, canvas.width / 2, canvas.height / 2);
-	
-
-// 				ctx.fillStyle = "grey";
-// 				ctx.beginPath();
-
-// 				for (let i = 0; i < canvas.height; i += 20)
-// 				{
-// 					if (i < 360 || i > 460)
-// 						ctx.rect(canvas.width / 2 - 5 / 2, i, 10, 10)
-// 				}
-// 				ctx.fill();
-
-// 				ctx.fillStyle = "white"
-// 				ctx.beginPath();
-
-// 				ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
-// 				ctx.fill();
-
-// 				ctx.rect(pad1X, pad1Y, pad1W, pad1H);
-// 				ctx.rect(pad2X, pad2Y, pad2W, pad2H);
-// 				ctx.fill();
-
-// 				return; 
-// 			}
-// 		}
-
-// 		draw_score(ctx, canvas, scoreP1, scoreP2);
-
-// 		ballX += speedX;
-//         ballY += speedY;
-
-//         if (keys["w"]) pad1Y -= padSpeed;
-//         if (keys["s"]) pad1Y += padSpeed;
-
-// 		// botPalyer();
-//         if (keys["ArrowUp"]) pad2Y -= padSpeed;
-//         if (keys["ArrowDown"]) pad2Y += padSpeed;
-
-//         pad1Y = Math.max(0, Math.min(canvas.height - pad1H, pad1Y))
-//         pad2Y = Math.max(0, Math.min(canvas.height - pad2H, pad2Y))
-
-//         if (!paused && !gameOver)
-// 		{
-// 			if (ballX >= canvas.width - ballRadius)
-//             	score(1);
-// 			if (ballX <= 0 + ballRadius)
-// 				score(2);
-// 		}
-
-//         if (ballY <= 0 + ballRadius || ballY >= canvas.height - ballRadius)
-//             speedY *= -1;
-
-//         if (ballX - ballRadius < pad1X + pad1W && 
-//             ballY > pad1Y && 
-//             ballY < pad1Y + pad1H
-//         )
-//         {
-//             let relativeIntersectY = (pad1Y + pad1H / 2) - ballY;
-
-//             let normalizedIntersectY = relativeIntersectY / (pad1H / 2);
-//             speedY = -normalizedIntersectY * maxBounceAngle;
-
-//             speedX *= -1;
-//             ballX = pad1X + pad1W + ballRadius;
-//         }
-
-//         if (ballX + ballRadius > pad2X && 
-//             ballY + ballRadius > pad2Y && 
-//             ballY + ballRadius < pad2Y + pad2H &&
-//             speedX > 0
-//         )
-//         {
-//             let relativeIntersectY = (pad2Y + pad2H / 2) - ballY;
-
-//             let normalizedIntersectY = relativeIntersectY / (pad2H / 2);
-//             speedY = -normalizedIntersectY * maxBounceAngle;
-
-//             speedX *= -1;
-//             ballX = pad2X - ballRadius;
-//         }
-
-//         ctx.fillStyle = "grey";
-//         ctx.beginPath();
-
-//         for (let i = 0; i < canvas.height; i += 20)
-//         {
-//             ctx.rect(canvas.width / 2 - 5 / 2, i, 10, 10)
-//         }
-// 		ctx.fill();
-
-// 		ctx.fillStyle = "white"
-// 		ctx.beginPath();
-
-//         ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
-//         ctx.fill();
-
-//         ctx.rect(pad1X, pad1Y, pad1W, pad1H);
-//         ctx.rect(pad2X, pad2Y, pad2W, pad2H);
-//         ctx.fill();
-//     }
-
-// 	function frame()
-// 	{
-//         if (!gameStarted)
-// 		{
-//             drawLoader();
-//         }
-// 		else
-// 		{
-//             drawGame();
-//         }
-//         requestAnimationFrame(frame);
-//     }
-
-// 	frame();
-// }
-  
