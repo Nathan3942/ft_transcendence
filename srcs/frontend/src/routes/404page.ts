2cf01d23054f2c3a0c6876b0d646be17840a7780
moop250 <hugo.libine@proton.me>
2026-01-14 16:23:52 +0100

    Seeing how darkmode works

--- srcs/frontend/src/routes/404page.ts
+++ srcs/frontend/src/routes/404page.ts
@@ -2,9 +2,9 @@ export default async function create404Page(): Promise<HTMLElement>  {
 	const template = document.createElement("template");
 	
 	template.innerHTML = `
-	<div class="w-full h-full flex flex-col items-center">
-		<h1 class="text-4xl font-bold text-center">404</h1>
-		<p class="text-lg text-center">Page Not Found</p>
+	<div class="w-full h-full flex flex-col items-center justify-center">
+		<h1 class="text-4xl font-bold text-center" id="error-title">404</h1>
+		<p class="text-lg text-center" id"error-text">Page Not Found</p>
 	</div>
 	`;
 
