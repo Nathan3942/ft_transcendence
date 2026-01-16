272f23b5e3d02dfd31ce4b4b4b4f5b4d32d74ec3
moop250 <hugo.libine@proton.me>
2026-01-16 19:23:06 +0100

    Updated footer

--- srcs/frontend/src/components/footer/footer.ts
+++ srcs/frontend/src/components/footer/footer.ts
@@ -3,7 +3,7 @@ export default function createFooter(): HTMLDivElement {
 	template.innerHTML = `
 		<footer class="fixed w-full bottom-0">
 			<div class="justify-center flex w-full bg-gray-200 dark:bg-gray-800 p-[20px] dark:text-white">
-				<div class="flex m-1">Copyright - I dislike CSS</div>
+				<div class="flex m-1">© hlibine & tmontani & njeanbou</div>
 			</div>
 		</footer>
 	`
