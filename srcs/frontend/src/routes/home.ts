9b316656bef709025400188b7b4c01ec677a506b
moop250 <hugo.libine@proton.me>
2026-01-23 12:19:07 +0100

    Finished integrating buttons into the home page

--- srcs/frontend/src/routes/home.ts
+++ srcs/frontend/src/routes/home.ts
@@ -10,33 +10,78 @@ export default function createHomePage(): HTMLDivElement {
 					<button id="local-button"></button>
 				</div>
 				<div class="w-11/12 hover:w-full bg-green-300 dark:bg-green-900 hover:brightness-90 dark:hover:brightness-130">
-					<button class="w-full h-full flex flex-row p-4"><img src="assets/images/globe-svgrepo-com.svg" alt="Icon" class="h-10 pr-2 dark:invert">Online Play</button>
+					<button id="online-button"></button>
 				</div>
 				<div class="w-11/12 hover:w-full bg-red-300 dark:bg-red-900 hover:brightness-90 dark:hover:brightness-130">
-					<button class="w-full h-full flex flex-row p-4"><img src="assets/images/square-poll-vertical-svgrepo-com.svg" alt="Icon" class="h-10 pr-2 dark:invert">Leaderboard</button>
+					<button id="leaderboard-button"></button>
 				</div>
 				<div class="w-11/12 hover:w-full bg-purple-300 dark:bg-purple-900 hover:brightness-90 dark:hover:brightness-130">
-					<button class="w-full h-full flex flex-row p-4"><img src="assets/images/circle-information-svgrepo-com.svg" alt="Icon" class="h-10 pr-2 dark:invert">About</button>
+					<button id="about-button"></button>
 				</div>
 			</div>
 		</div>
 	`;
 
+	const buttonClasses: string = "w-full h-full flex flex-row p-4";
+	const iconBClasses: string = "h-10 pr-2 dark:invert";
+
 	const localButton = createButton({
 		id: "local-button",
-		type: "button",
-		extraClasses: "w-full h-full flex flex-row p-4",
+		extraClasses: buttonClasses,
 		buttonText: "Local Play",
 		icon: "assets/images/monitor-svgrepo-com.svg",
 		iconAlt: "Icon",
-		iconBClass: "h-10 pr-2 dark:invert",
+		iconBClass: iconBClasses,
 		href: "/game-local"
 	});
 
 	const localButtonInsert = template.content.querySelector("#local-button");
-	if (localButtonInsert) {
+	if (localButtonInsert)
 		localButtonInsert.replaceWith(localButton);
-	}
+
+
+	const onlineButton = createButton({
+		id: "online-button",
+		extraClasses: buttonClasses,
+		buttonText: "Online Play",
+		icon: "assets/images/globe-svgrepo-com.svg",
+		iconAlt: "Icon",
+		iconBClass: iconBClasses,
+		href: "/game-online"
+	});
+
+	const onlineButtonInsert = template.content.querySelector("#online-button");
+	if (onlineButtonInsert)
+		onlineButtonInsert.replaceWith(onlineButton);
+
+	const leaderboardButton = createButton({
+		id: "leaderboard-button",
+		extraClasses: buttonClasses,
+		buttonText: "Leaderboard",
+		icon: "assets/images/square-poll-vertical-svgrepo-com.svg",
+		iconAlt: "Icon",
+		iconBClass: iconBClasses,
+		href: "/leaderboard"
+	});
+
+	const leaderboardButtonInsert = template.content.querySelector("#leaderboard-button");
+	if (leaderboardButtonInsert)
+		leaderboardButtonInsert.replaceWith(leaderboardButton);
+
+
+	const aboutButton = createButton({
+		id: "about-button",
+		extraClasses: buttonClasses,
+		buttonText: "About",
+		icon: "assets/images/circle-information-svgrepo-com.svg",
+		iconAlt: "Icon",
+		iconBClass: iconBClasses,
+		href: "/about"
+	});
+
+	const aboutButtonInsert = template.content.querySelector("#about-button");
+	if (aboutButtonInsert)
+		aboutButtonInsert.replaceWith(aboutButton);
 
 	return template.content.firstElementChild as HTMLDivElement;
 }
