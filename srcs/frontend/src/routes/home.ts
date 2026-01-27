44b1b7c901cf47ea49f0c0b98203c9cd7ad57582
moop250 <hugo.libine@proton.me>
2026-01-27 13:11:21 +0100

    refactored Homepage to fit new standard

--- srcs/frontend/src/routes/home.ts
+++ srcs/frontend/src/routes/home.ts
@@ -1,87 +1,57 @@
 import { createButton } from "../components/button/button";
+import makeButtonBlock from "../components/button/buttonBlock";
 
 export default function createHomePage(): HTMLDivElement {
 
-	const template = document.createElement("template");
-	template.innerHTML = `
-		<div class="flex flex-1 flex-col justify-center items-end">
-			<div class="text-3xl w-9/12 h-2/3 flex flex-col items-end justify-between">
-				<div class="w-11/12 hover:w-full bg-blue-300 dark:bg-blue-900 hover:brightness-90 dark:hover:brightness-130">
-					<button id="local-button"></button>
-				</div>
-				<div class="w-11/12 hover:w-full bg-green-300 dark:bg-green-900 hover:brightness-90 dark:hover:brightness-130">
-					<button id="online-button"></button>
-				</div>
-				<div class="w-11/12 hover:w-full bg-red-300 dark:bg-red-900 hover:brightness-90 dark:hover:brightness-130">
-					<button id="leaderboard-button"></button>
-				</div>
-				<div class="w-11/12 hover:w-full bg-purple-300 dark:bg-purple-900 hover:brightness-90 dark:hover:brightness-130">
-					<button id="about-button"></button>
-				</div>
-			</div>
-		</div>
-	`;
+	const outer = document.createElement("div");
+	const inner = document.createElement("div");
+
+	outer.className = "flex flex-1 flex-col justify-center items-end";
+	inner.className = "text-3xl w-9/12 h-2/3 flex flex-col items-end justify-between";
 
 	const buttonClasses: string = "w-full h-full flex flex-row p-4";
 	const iconBClasses: string = "h-10 pr-2 dark:invert";
 
-	const localButton = createButton({
-		id: "local-button",
-		extraClasses: buttonClasses,
-		buttonText: "Local Play",
-		icon: "assets/images/monitor-svgrepo-com.svg",
-		iconAlt: "Icon",
-		iconBClass: iconBClasses,
-		href: "/game-local"
-	});
+	inner.append(
+		makeButtonBlock("bg-blue-300 dark:bg-blue-900", createButton({
+			id: "local-button",
+			extraClasses: buttonClasses,
+			buttonText: "Local Play",
+			icon: "assets/images/monitor-svgrepo-com.svg",
+			iconAlt: "Icon",
+			iconBClass: iconBClasses,
+			href: "/game-local"
+		})),
+		makeButtonBlock("bg-green-300 dark:bg-green-900", createButton({
+			id: "online-button",
+			extraClasses: buttonClasses,
+			buttonText: "Online Play",
+			icon: "assets/images/globe-svgrepo-com.svg",
+			iconAlt: "Icon",
+			iconBClass: iconBClasses,
+			href: "/game-online"
+		})),
+		makeButtonBlock("bg-red-300 dark:bg-red-900", createButton({
+			id: "leaderboard-button",
+			extraClasses: buttonClasses,
+			buttonText: "Leaderboard",
+			icon: "assets/images/square-poll-vertical-svgrepo-com.svg",
+			iconAlt: "Icon",
+			iconBClass: iconBClasses,
+			href: "/leaderboard"
+		})),
+		makeButtonBlock("bg-purple-300 dark:bg-purple-900", createButton({
+			id: "about-button",
+			extraClasses: buttonClasses,
+			buttonText: "About",
+			icon: "assets/images/circle-information-svgrepo-com.svg",
+			iconAlt: "Icon",
+			iconBClass: iconBClasses,
+			href: "/about"
+		}))
+	)
 
-	const localButtonInsert = template.content.querySelector("#local-button");
-	if (localButtonInsert)
-		localButtonInsert.replaceWith(localButton);
+	outer.append(inner);
 
-
-	const onlineButton = createButton({
-		id: "online-button",
-		extraClasses: buttonClasses,
-		buttonText: "Online Play",
-		icon: "assets/images/globe-svgrepo-com.svg",
-		iconAlt: "Icon",
-		iconBClass: iconBClasses,
-		href: "/game-online"
-	});
-
-	const onlineButtonInsert = template.content.querySelector("#online-button");
-	if (onlineButtonInsert)
-		onlineButtonInsert.replaceWith(onlineButton);
-
-	const leaderboardButton = createButton({
-		id: "leaderboard-button",
-		extraClasses: buttonClasses,
-		buttonText: "Leaderboard",
-		icon: "assets/images/square-poll-vertical-svgrepo-com.svg",
-		iconAlt: "Icon",
-		iconBClass: iconBClasses,
-		href: "/leaderboard"
-	});
-
-	const leaderboardButtonInsert = template.content.querySelector("#leaderboard-button");
-	if (leaderboardButtonInsert)
-		leaderboardButtonInsert.replaceWith(leaderboardButton);
-
-
-	const aboutButton = createButton({
-		id: "about-button",
-		extraClasses: buttonClasses,
-		buttonText: "About",
-		icon: "assets/images/circle-information-svgrepo-com.svg",
-		iconAlt: "Icon",
-		iconBClass: iconBClasses,
-		href: "/about"
-	});
-
-	const aboutButtonInsert = template.content.querySelector("#about-button");
-	if (aboutButtonInsert)
-		aboutButtonInsert.replaceWith(aboutButton);
-
-	return template.content.firstElementChild as HTMLDivElement;
+	return outer;
 }
