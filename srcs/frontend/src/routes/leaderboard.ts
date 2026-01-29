1b37da460bd4876227782e7b1c7ad0ea573d3cb1
moop250 <hugo.libine@proton.me>
2026-01-29 15:05:33 +0100

    restarting the leaderboard

--- srcs/frontend/src/routes/leaderboard.ts
+++ srcs/frontend/src/routes/leaderboard.ts
@@ -3,9 +3,9 @@ function createPodium(genClasses: string, podClasses: string, id: string): HTMLD
 	const column = document.createElement("div");
 	const img = document.createElement("img");
 
-	div.className = genClasses + " " + "flex flex-col w-1/4 justify-center items-center" ;
+	div.className = genClasses + " " + "flex flex-col w-1/3 justify-center items-center" ;
 
-	img.className = "w-1/2 w-max-1/2 h-max-1/2 aspect-[1/1]"
+	img.className = "w-1/2 w-max-1/2 h-max-1/2 aspect-[1/1] dark:invert"
 	img.src = "assets/images/user-svgrepo-com.svg"
 	column.className = podClasses + " " + "flex h-full w-1/2 items-center justify-center"
 	column.append(id) 
@@ -16,18 +16,12 @@ function createPodium(genClasses: string, podClasses: string, id: string): HTMLD
 
 export default function buildLeaderboardPage(): HTMLDivElement {
 	const outer = document.createElement("div");
-	const podium = document.createElement("div");
 	const leaderboard = document.createElement("div");
 
 	outer.className = "flex flex-1 flex-col items-center";
-	podium.className = "flex w-1/3 columns-3 justify-center items-end h-1/2 m-6"
-	podium.append(
-		createPodium("h-5/6", "bg-gray-500", "2"),
-		createPodium("h-full", "bg-yellow-900", "1"),
-		createPodium("h-2/3", "bg-orange-900", "3")
-	)
+	leaderboard.className = "w-3/4"
 
-	outer.append(podium, leaderboard);
+	outer.append(leaderboard);
 
 	return outer;
 }
