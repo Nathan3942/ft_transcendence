924450dd35c40a3201bda56cfbe4c04fd45d0e89
moop250 <hugo.libine@proton.me>
2026-02-02 17:32:41 +0100

    making a proper skeleton to be filled out

--- srcs/frontend/src/routes/leaderboard.ts
+++ srcs/frontend/src/routes/leaderboard.ts
@@ -1,27 +1,42 @@
-function createPodium(genClasses: string, podClasses: string, id: string): HTMLDivElement {
-	const div = document.createElement("div");
-	const column = document.createElement("div");
-	const img = document.createElement("img");
-
-	div.className = genClasses + " " + "flex flex-col w-1/3 justify-center items-center" ;
-
-	img.className = "w-1/2 w-max-1/2 h-max-1/2 aspect-[1/1] dark:invert"
-	img.src = "assets/images/user-svgrepo-com.svg"
-	column.className = podClasses + " " + "flex h-full w-1/2 items-center justify-center"
-	column.append(id) 
-
-	div.append(img, column)
-	return div;
-}
+import createBackButton from "../components/button/backButton";
+import { createButton } from "../components/button/button";
 
 export default function buildLeaderboardPage(): HTMLDivElement {
+	
+	function buildOnlineLeaderboard(): HTMLDivElement {
+		const leaderboard = document.createElement("div");
+
+		return leaderboard;
+	}
+
+	function buildOfflineLeaderboard(): HTMLDivElement {
+		const leaderboard = document.createElement("div");
+
+		return leaderboard;
+	}
+
+	function buildBoardChooser(): HTMLDivElement {
+		const buttons = document.createElement("div");
+		buttons.append(createButton({
+				f: () => buildOnlineLeaderboard(),
+			}),
+			createButton({
+				f: () => buildOfflineLeaderboard(),
+			})
+		)
+
+		return buttons;
+	}
+	
 	const outer = document.createElement("div");
-	const leaderboard = document.createElement("div");
+	const boardChooser: HTMLDivElement = buildBoardChooser();
+	const leaderboard: HTMLDivElement = buildOnlineLeaderboard();
+	
+	outer.className = "flex flex-1 flex-col";
+	
+	const backButton = createBackButton("", "/")
 
-	outer.className = "flex flex-1 flex-col items-center";
-	leaderboard.className = "w-3/4"
-
-	outer.append(leaderboard);
+	outer.append(boardChooser, leaderboard, backButton);
 
 	return outer;
 }
