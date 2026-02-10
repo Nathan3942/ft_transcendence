cde17b76e2b59fe59c3c3a925cb794bff45b048a
tmontani25 <teomontani27@gmail.com>
2026-02-10 15:13:38 +0100

    rm migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,14 +2,5 @@
 
 import { initDatabase, getDatabase, checkDatabaseHealth } from './connection'
 import { initTables } from './init'
-import { runMigrations } from './migrations'
 
-export { initDatabase, getDatabase, checkDatabaseHealth, initTables, runMigrations }
-
-export default { // contains all db functions but unlikely to use
-  initDatabase,
-  getDatabase,
-  checkDatabaseHealth,
-  initTables,
-  runMigrations,
-}
+export { initDatabase, getDatabase, checkDatabaseHealth, initTables }
