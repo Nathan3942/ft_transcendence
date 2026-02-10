08f7195b2d89bf597513d56b7dc702de34c881e1
tmontani25 <teomontani27@gmail.com>
2026-02-10 17:27:30 +0100

    front avec appels api

--- srcs/database/index.ts
+++
@@ -1,6 +0,0 @@
-
-
-import { initDatabase, getDatabase, checkDatabaseHealth } from './connection'
-import { initTables } from './init'
-
-export { initDatabase, getDatabase, checkDatabaseHealth, initTables }
--- srcs/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-/* ce fichier permet de determiner les elements que doit contenir
-un user dans mon systeme, on cree en quelque sorte un type user
-contrat de forme
-*/
-
-export interface User {
-  id: number; 
-  username: string;
-}
