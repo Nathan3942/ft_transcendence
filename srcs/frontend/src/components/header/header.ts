18ea339c421875b0a5c94fe11efcf458c979fa24
moop250 <hugo.libine@proton.me>
2026-02-19 16:53:29 +0100

    Working on dropdown menu

--- srcs/frontend/src/components/header/header.ts
+++ srcs/frontend/src/components/header/header.ts
@@ -1,3 +1,4 @@
+import { logoutHandler } from "../../handler/loginHandler";
 import { createButton } from "../button/button";
 
 export default function createHeader(): HTMLHeadElement {
@@ -5,6 +6,7 @@ export default function createHeader(): HTMLHeadElement {
 	const header = document.createElement("header");
 	const title = document.createElement("div");
 	const navbar = document.createElement("div");
+	const userDropdown = document.createElement("div");
 
 	header.className = "min-h-15 h-15 flex justify-between align-middle items-center bg-gray-200 dark:bg-gray-800";
 	
@@ -15,8 +17,38 @@ export default function createHeader(): HTMLHeadElement {
 		href: "/"
 	}))
 
+	userDropdown.className = "invisible opacity-0 absolute right-0 mt-2 px-2 bg-white dark:bg-gray-700 shadow-lg py-2 transition-opacity duration-150 group-hover:visible group-hover:opacity-100 z-10";
+	userDropdown.append((
+		createButton({
+			id: "user-profile-button",
+			href: "/user-profile",
+			buttonText: "Profile"
+		})),
+		createButton({
+			id: "user-settings-button",
+			href: "/user-settings",
+			buttonText: "Settings"
+		}),
+		createButton({
+			id: "logout-button",
+			f: () => logoutHandler(),
+			buttonText: "Logout"
+		})
+	);
+
 	navbar.className = "flex justify-end space-x-5 px-4";
 	navbar.id = "navbar";
+	const userProfileBtn = createButton({
+			id: "user-profile-dropdown",
+			extraClasses: "relative group",
+			// f: () => show user menu dropdown
+			icon: "assets/images/user-svgrepo-com.svg",
+			iconAlt: "Icon",
+			iconBClass: "h-8 dark:invert"
+		});
+
+	userProfileBtn.append(userDropdown);
+
 	navbar.append(
 		createButton({
 			id: "friend-menu-button",
@@ -32,14 +64,8 @@ export default function createHeader(): HTMLHeadElement {
 			iconAlt: "Icon",
 			iconBClass: "h-8 dark:invert"
 		}),
-		createButton({
-			id: "user-profile-button",
-			// f: () => show user menu dropdown
-			icon: "assets/images/user-svgrepo-com.svg",
-			iconAlt: "Icon",
-			iconBClass: "h-8 dark:invert"
-		})
-	)
+		userProfileBtn
+	);
 	
 	header.append(title, navbar);
 	
