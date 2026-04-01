968c93c1d69e7a69c0f6537f00fcae8ec6caa5e3
moop250 <hugo.libine@proton.me>
2026-04-01 16:30:58 +0200

    updated frontend vite config to allow navigation to api though search bar

--- srcs/frontend/vite.config.js
+++ srcs/frontend/vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   },
   plugins: [
     tailwindcss(),
